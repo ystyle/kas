3863b4ecadd2128e900040a78f01ee45084fa255
YSTYLE <lxy5266@live.com>
2020-03-11 01:36:21 +0800

    去掉不必要的打印日志

--- main.go
+++ main.go
@@ -18,10 +18,76 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"strings"
 	"time"
 )
 
-var upgrader = websocket.Upgrader{}
+var (
+	upgrader     = websocket.Upgrader{}
+	loggerConfig = middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: func(c echo.Context) bool {
+			if c.Path() == "/ping" {
+				return true
+			}
+			if strings.HasPrefix(c.Path(), "/asset/") {
+				return true
+			}
+			return false
+		},
+	})
+)
+
+func init() {
+	file.CheckDir(config.StoreDir)
+}
+
+func main() {
+	log.EnableColor()
+	if os.Getenv("MODE") == "DEBUG" {
+		log.SetLevel(log.DEBUG)
+		log.Info("log level: Debug")
+	}
+	wm := core.GetWsManager()
+	// hcomic
+	wm.RegisterService("hcomic:submit", services.Submit)
+	// text to epub / mobi
+	wm.RegisterService("text:upload", services.TextUpload)
+	wm.RegisterService("text:preview", services.TextPreView)
+	wm.RegisterService("text:convert", services.TextConvert)
+	wm.RegisterService("text:download", services.TextDownload)
+	// aricle
+	wm.RegisterService("article:submit", services.ArticleSubmit)
+	// ping
+	wm.RegisterService("ping", services.Ping)
+	// 注册设备
+	wm.RegisterService("regsiter", services.Register)
+
+	e := echo.New()
+	e.Use(loggerConfig)
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Gzip())
+
+	box := rice.MustFindBox("public")
+	assetHandler := http.FileServer(box.HTTPBox())
+	site.Init(box)
+
+	e.GET("/*", echo.WrapHandler(assetHandler))
+	e.GET("/asset/*", echo.WrapHandler(assetHandler))
+	e.Static("/download", "storage")
+	e.GET("/ws", WS)
+	e.GET("/ws#", WS)
+	e.GET("ping", ping)
+
+	// 打印服务器负载
+	go PrintStatistics()
+
+	if runtime.GOOS == "windows" {
+		dir := path.Dir(os.Args[0])
+		hcomic.Run(dir, "cmd", "/c", "start", "http://127.0.0.1:1323")
+	}
+	e.Logger.Fatal(e.Start(":1323"))
+}
 
 func WS(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
@@ -31,14 +97,6 @@ func WS(c echo.Context) error {
 	wm := core.GetWsManager()
 	wm.Add(core.NewWsClient(ws, c.Request()))
 	return nil
-}
-
-func init() {
-	createStoreDir()
-}
-
-func createStoreDir() {
-	file.CheckDir(config.StoreDir)
 }
 
 func PrintStatistics() {
@@ -69,49 +127,6 @@ func PrintStatistics() {
 	}
 }
 
-func main() {
-	log.EnableColor()
-	if os.Getenv("MODE") == "DEBUG" {
-		log.SetLevel(log.DEBUG)
-		log.Info("log level: Debug")
-	}
-	wm := core.GetWsManager()
-	// hcomic
-	wm.RegisterService("hcomic:submit", services.Submit)
-	// text to epub / mobi
-	wm.RegisterService("text:upload", services.TextUpload)
-	wm.RegisterService("text:preview", services.TextPreView)
-	wm.RegisterService("text:convert", services.TextConvert)
-	wm.RegisterService("text:download", services.TextDownload)
-	// aricle
-	wm.RegisterService("article:submit", services.ArticleSubmit)
-	// ping
-	wm.RegisterService("ping", services.Ping)
-	// 注册设备
-	wm.RegisterService("regsiter", services.Register)
-
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.RequestID())
-	e.Use(middleware.Gzip())
-
-	box := rice.MustFindBox("public")
-	assetHandler := http.FileServer(box.HTTPBox())
-	site.Init(box)
-
-	e.GET("/*", echo.WrapHandler(assetHandler))
-	e.GET("/asset/*", echo.WrapHandler(assetHandler))
-	e.Static("/download", "storage")
-	e.GET("/ws", WS)
-	e.GET("/ws#", WS)
-
-	// 打印服务器负载
-	go PrintStatistics()
-
-	if runtime.GOOS == "windows" {
-		dir := path.Dir(os.Args[0])
-		hcomic.Run(dir, "cmd", "/c", "start", "http://127.0.0.1:1323")
-	}
-	e.Logger.Fatal(e.Start(":1323"))
+func ping(c echo.Context) error {
+	return c.String(http.StatusOK, "pong")
 }

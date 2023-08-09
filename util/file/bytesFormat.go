98c6e7a590cf717ac8a4ce39ee3f4d4370b67418
YSTYLE-L.X.Y <lxy5266@live.com>
2023-08-09 22:57:35 +0800

    feat: 修改打包脚本

--- util/file/bytesFormat.go
+++ util/file/bytesFormat.go
@@ -7,9 +7,7 @@ func FormatBytesLength(length int) string {
 		return fmt.Sprintf("%d K", length/(1024))
 	} else if length < 1024*1024*1024 {
 		return fmt.Sprintf("%d M", length/(1024*1024))
-	} else if length < 1024*1024*1024*1024 {
-		return fmt.Sprintf("%d G", length/(1024*1024*1024))
 	} else {
-		return fmt.Sprintf("%d T", length/(1024*1024*1024*1024))
+		return fmt.Sprintf("%d G", length/(1024*1024*1024))
 	}
 }
